27393cad99e92792ae506b325436993bae9fddd3
alexfmejia96 <65797917+alexfmejia96@users.noreply.github.com>
2023-04-17 20:29:57 +0000

    Web App v0.5

---
+++ Home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Group 7: Book Recommender",
+    page_icon="books",
+)
+
+
+st.title("Book Recommandation Web App")
+st.text("Welcome to our web app, designed to provide personalized book recommendations.")
+
+
